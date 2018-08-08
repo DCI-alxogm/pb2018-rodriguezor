ac8cf57ae6438ac1f3cbc547345e968cbdc23e36
Rodriguez Ojeda Ricardo Alberto <ra.rodriguezojeda@ugto.mx>
2018-08-08 09:27:55 -0500

    Este es mi primer programa y mi comentario

---
+++ semana1/primer_programa.c
@@ -0,0 +1,9 @@
+/*Creado el 8 de agosto de 2018 por Ricardo Rodriguez*/
+#include<stdio.h>
+int main()
+{
+printf("Hola mundo, estoy sufriendo en esta clase"\n");
+printf("Este es el primer programa que hare en este curso\n");
+}
+/*Termine de escribir el programa*/
+
---
+++ semana1/programa_hola.c
@@ -0,0 +1,2 @@
+/*Este es un archivo creado por mí, donde me doy cuenta que la programacion no es lo mio, pero vamos a ver que resulta “hola mundo a la pantalla”
+Creado el 08/08/2018/*
