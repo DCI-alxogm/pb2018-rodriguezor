be1ab5e14dcd9c817e3b10c49bae2141dfd01877
Rodriguez Ojeda Ricardo Alberto <ra.rodriguezojeda@ugto.mx>
2018-08-15 08:17:28 -0500

    Nueva actualizacion

--- semana1/primer_programa.c
+++ semana1/primer_programa.c
@@ -2,7 +2,7 @@
 #include<stdio.h>
 int main()
 {
-printf("Hola mundo, estoy sufriendo en esta clase"\n");
+printf("Hola mundo, estoy sufriendo en esta clase\n");
 printf("Este es el primer programa que hare en este curso\n");
 }
 /*Termine de escribir el programa*/
